6b6cd4809a3d6a513e5b415abb7c728b5bf5d9f8
UB Space Bulls <sbrover@SBRover.(none)>
2014-04-29 14:04:30 -0400

    Drive motors running

---
+++ src/serialpy/scripts/driveserial.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+import serial
+
+
+com = serial.Serial('/dev/ttyACM0',baudrate=115200)
+def callback(data):
+        data = str(data)
+        com.write(data)
+        com.flush()
+
+def controller():
+        rospy.init_node('drv')
+        rospy.Subscriber('/DRV', String, callback)
+        rospy.spin()
+
+if __name__ == '__main__':
+        controller()
+
