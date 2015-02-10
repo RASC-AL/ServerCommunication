d21ca6435b5e3485b92241225b57c5f80d7c0bf3
mrathbun <mrathbun@buffalo.edu>
2015-02-09 20:06:07 -0500

    Overhauled the Xbox controls for the drive train and arm

---
+++ catkin_ws/src/serialpy/scripts/relay.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+import serial
+
+com = serial.Serial('/dev/ttyS0',baudrate=115200)
+
+def callback(data):
+        data = 'r,'+str(data) + '\n'
+        com.write(data)
+        com.flush()
+
+def controller():
+        rospy.init_node('relay')
+        rospy.Subscriber('/RLY', String, callback)
+        rospy.spin()
+
+if __name__ == '__main__':
+        controller()
+
