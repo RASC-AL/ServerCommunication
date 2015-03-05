988040f696e4ac3d8c34a2a568fccc48da654edf
mrathbun <mrathbun@buffalo.edu>
2015-03-04 22:50:51 -0500

    Created outline for local control serial code

---
+++ src/serialpy/scripts/ControlSerial.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+import serial
+
+Command = ""
+
+def callbackArm(data):
+  global Command
+  if Command == "":
+    Command = 'l'+str(data)
+
+def callbackDrv(data):
+  global Command
+  if Command != "":
+    Command += str(data)
+    #Write to serial
+    Command = ""  
+  
+def controller():
+  rospy.init_node("ControlSerial")
+  rospy.Subscriber('ARM', String, callbackArm)
+  rospy.Subscriber('DRV', String, callbackDrv)
+  rospy.spin();
+
+if __name__ == '__main__':
+  controller()
