b891ce613eb58b8855c16b81831058bb1cc6c494
mrathbun <mrathbun@buffalo.edu>
2015-03-21 17:03:26 -0400

    Modified serial read publisher to catch interrupt exceptions

--- src/serialpy/scripts/serialRead.py
+++ src/serialpy/scripts/serialRead.py
@@ -3,16 +3,24 @@ import rospy
 from std_msgs.msg import String
 import serial
 
-#Setup
-ser = serial.Serial('/dev/ttyACM0', baudrate = 115200, timeout = .5)
-roverData_pub = rospy.Publisher('RoverData', String, queue_size = 1)
-rospy.init_node("SerialRead")
-data = ""
-rospy.sleep(5)
-r = rospy.Rate(16)
 
-#Loop
-while not rospy.is_shutdown():
-        data = ser.readline()
-      	roverData_pub.publish(data)
-        r.sleep()
+
+#Setup
+def talker():
+	ser = serial.Serial('/dev/ttyACM0', baudrate = 115200, timeout = .5)
+	roverData_pub = rospy.Publisher('RoverData', String, queue_size = 1)
+	rospy.init_node("SerialRead")
+	r = rospy.Rate(16)
+ 	rospy.sleep(5)
+	#Loop
+	while not rospy.is_shutdown():
+        	data = ser.readline()
+      		roverData_pub.publish(data)
+        	r.sleep()
+
+if __name__ == '__main__':
+	try:
+		talker()
+	except rospy.ROSInterruptException:
+		pass
+
